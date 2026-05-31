9e99beee1d0c8618e23be135370093c8c1d52aa9
Pierre du Plessis <pierre@pcservice.co.za>
2026-05-31 18:35:07 +0200

    feat: embed InlineRateEdit in project show page rate display

--- src/Form/InlineRateType.php
+++ src/Form/InlineRateType.php
@@ -19,6 +19,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 
+/**
+ * @extends AbstractType<mixed>
+ */
 final class InlineRateType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
